09b2675d39e05f75b3dad502788f72a5aa8efc04
Uriel Chinemerem Vit-Ojiegbe <uvit@meta.com>
2025-06-10 14:40:30 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+let test = import.meta.env.VITE_API_KEY
+
 const App = () => {
   return (
     <div className="App">
-    
+      API Key: ${test}
     </div>
   )
 }
